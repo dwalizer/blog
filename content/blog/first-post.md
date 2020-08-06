c32940290164102fd85de28c7f98ed3968b8ab26
Don Walizer <walizer.don@gmail.com>
2020-08-06 12:27:25 -0400

    Initial commit

---
+++ content/blog/first-post.md
@@ -0,0 +1,5 @@
+---
+title: My First Post
+---
+
+Testing this new blog
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Don Walizer Jr`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Don Walizer Jr`,
+      summary: `from Baltimore`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
